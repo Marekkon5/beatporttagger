464e6bfaa0b54dbcc616db427ebd0f78947553bb
Marekkon5 <marekkon5@yahoo.com>
2021-02-06 16:49:10 +0100

    Revert

--- beatport.py
+++ beatport.py
@@ -118,8 +118,7 @@ class Track:
         #Without remix/version stuff
         self.name = data['name']
         #Full title
-        # self.title = data['title']
-        self.title = f"{data['name']} ({data['mix']})"
+        self.title = data['title']
         self.release_date = datetime.datetime.strptime(data['date']['released'], '%Y-%m-%d')
         self.publish_date = datetime.datetime.strptime(data['date']['published'], '%Y-%m-%d')
 
